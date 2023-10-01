4ff081c507f1b8c04740d09be024a25071910484
Bert Klop <bert.klop@ns.nl>
2023-10-01 11:09:37 +0200

    api gateway with Lambda

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.17"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.3.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
   }
   required_version = ">= 1.5"
 }
